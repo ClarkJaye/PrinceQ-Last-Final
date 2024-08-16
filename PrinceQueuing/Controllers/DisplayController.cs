575494ca23db08fd69346ba70be74f9cef5c55ac
ICTDEV OJT <ictdev@princeretail.com>
2024-08-16 17:08:39 +0800

    done

--- PrinceQueuing/Controllers/DisplayController.cs
+++ PrinceQueuing/Controllers/DisplayController.cs
@@ -1,10 +1,7 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
-using Microsoft.EntityFrameworkCore;
 using PrinceQ.DataAccess.Hubs;
 using PrinceQ.DataAccess.Repository;
-using System.Management;
 
 namespace PrinceQueuing.Controllers
 {
